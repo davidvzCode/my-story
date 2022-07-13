47d840b4c395d611fb2a8badb41b99dc71be13ea
davidvzCode <davidvz.code@gmail.com>
2022-07-12 23:23:51 -0500

    fixed migrations

--- db/migrations/20220712021951-create-customers.js
+++ db/migrations/20220712021951-create-customers.js
@@ -1,11 +1,48 @@
 'use strict';
 
-
-const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
+const { DataTypes, Sequelize} = require("sequelize");
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const { USER_TABLE } = require('../models/user.model');
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, {
+      id: {
+        allowNull: true,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+    },
+    name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    lastname: {
+        allowNull: false,
+        type: DataTypes.STRING,
+    },
+    phone: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW
+    },
+    userId:{
+        field: 'user_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+            model: USER_TABLE,
+            key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+    }
+    });
   },
 
   async down (queryInterface) {
